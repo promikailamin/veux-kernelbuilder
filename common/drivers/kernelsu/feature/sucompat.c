d08a92c5b8a1ef6f678d6fe0b24f6c057004998e
MIKAILAMIN <mikailamin.freefire@gmail.com>
2026-07-21 18:32:28 +0600

    drivers/kernelsu: use arch-safe pgtable includes

---
+++ common/drivers/kernelsu/feature/sucompat.c
@@ -0,0 +1,6 @@
+--- a/common/drivers/kernelsu/feature/sucompat.c
++++ b/common/drivers/kernelsu/feature/sucompat.c
+@@
+-#include <linux/pgtable.h>
++#include <linux/mm.h>
++#include <asm/pgtable.h>
